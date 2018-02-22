44577fb1d8f9a20130c827992691acad8bd42a78
Guoyongchang@WinToGo <guoyongchang@saohuo77.com>
2018-02-22 12:59:36 +0800

    '打包通过'

---
+++ FileUpdater/Updater.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace FileUpdater {
+  partial class Updater {
+    /// <summary>
+    /// 必需的设计器变量。
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// 清理所有正在使用的资源。
+    /// </summary>
+    /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows 窗体设计器生成的代码
+
+    /// <summary>
+    /// 设计器支持所需的方法 - 不要修改
+    /// 使用代码编辑器修改此方法的内容。
+    /// </summary>
+    private void InitializeComponent() {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Updater));
+      this.UpdateBtn = new System.Windows.Forms.Button();
+      this.LatestVersionLabel = new System.Windows.Forms.Label();
+      this.CurrentVersionLabel = new System.Windows.Forms.Label();
+      this.lbl_DownloadName = new System.Windows.Forms.Label();
+      this.UpdatePgb = new System.Windows.Forms.ProgressBar();
+      this.TitleLbl = new System.Windows.Forms.Label();
+      this.lbl_CloseApplication = new System.Windows.Forms.Label();
+      this.HeadBgd = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // UpdateBtn
+      // 
+      this.UpdateBtn.Location = new System.Drawing.Point(125, 33);
+      this.UpdateBtn.Name = "UpdateBtn";
+      this.UpdateBtn.Size = new System.Drawing.Size(116, 32);
+      this.UpdateBtn.TabIndex = 18;
+      this.UpdateBtn.Text = "Check|Fix";
+      this.UpdateBtn.UseVisualStyleBackColor = true;
+      this.UpdateBtn.Click += new System.EventHandler(this.UpdateBtn_Click);
+      // 
+      // LatestVersionLabel
+      // 
+      this.LatestVersionLabel.AutoSize = true;
+      this.LatestVersionLabel.ForeColor = System.Drawing.Color.PaleGreen;
+      this.LatestVersionLabel.Location = new System.Drawing.Point(10, 49);
+      this.LatestVersionLabel.Name = "LatestVersionLabel";
+      this.LatestVersionLabel.Size = new System.Drawing.Size(83, 12);
+      this.LatestVersionLabel.TabIndex = 15;
+      this.LatestVersionLabel.Text = "LatestVersion";
+      // 
+      // CurrentVersionLabel
+      // 
+      this.CurrentVersionLabel.AutoSize = true;
+      this.CurrentVersionLabel.ForeColor = System.Drawing.Color.PaleGreen;
+      this.CurrentVersionLabel.Location = new System.Drawing.Point(10, 33);
+      this.CurrentVersionLabel.Name = "CurrentVersionLabel";
+      this.CurrentVersionLabel.Size = new System.Drawing.Size(89, 12);
+      this.CurrentVersionLabel.TabIndex = 16;
+      this.CurrentVersionLabel.Text = "CurrentVersion";
+      // 
+      // lbl_DownloadName
+      // 
+      this.lbl_DownloadName.AutoSize = true;
+      this.lbl_DownloadName.ForeColor = System.Drawing.Color.PaleGreen;
+      this.lbl_DownloadName.Location = new System.Drawing.Point(10, 65);
+      this.lbl_DownloadName.Name = "lbl_DownloadName";
+      this.lbl_DownloadName.Size = new System.Drawing.Size(53, 12);
+      this.lbl_DownloadName.TabIndex = 17;
+      this.lbl_DownloadName.Text = "FileName";
+      this.lbl_DownloadName.Visible = false;
+      // 
+      // UpdatePgb
+      // 
+      this.UpdatePgb.Location = new System.Drawing.Point(12, 81);
+      this.UpdatePgb.Name = "UpdatePgb";
+      this.UpdatePgb.Size = new System.Drawing.Size(228, 15);
+      this.UpdatePgb.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+      this.UpdatePgb.TabIndex = 14;
+      this.UpdatePgb.Value = 100;
+      // 
+      // TitleLbl
+      // 
+      this.TitleLbl.AutoSize = true;
+      this.TitleLbl.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(73)))), ((int)(((byte)(120)))));
+      this.TitleLbl.ForeColor = System.Drawing.Color.DarkOrange;
+      this.TitleLbl.Location = new System.Drawing.Point(78, 10);
+      this.TitleLbl.Name = "TitleLbl";
+      this.TitleLbl.Size = new System.Drawing.Size(95, 12);
+      this.TitleLbl.TabIndex = 13;
+      this.TitleLbl.Text = "BigcraftUpdater";
+      this.TitleLbl.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+      this.TitleLbl.MouseDown += new System.Windows.Forms.MouseEventHandler(this.HeadBgd_MouseDown);
+      this.TitleLbl.MouseMove += new System.Windows.Forms.MouseEventHandler(this.HeadBgd_MouseMove);
+      // 
+      // lbl_CloseApplication
+      // 
+      this.lbl_CloseApplication.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(73)))), ((int)(((byte)(120)))));
+      this.lbl_CloseApplication.ForeColor = System.Drawing.Color.Transparent;
+      this.lbl_CloseApplication.Image = ((System.Drawing.Image)(resources.GetObject("lbl_CloseApplication.Image")));
+      this.lbl_CloseApplication.Location = new System.Drawing.Point(225, 7);
+      this.lbl_CloseApplication.Name = "lbl_CloseApplication";
+      this.lbl_CloseApplication.Size = new System.Drawing.Size(16, 16);
+      this.lbl_CloseApplication.TabIndex = 12;
+      this.lbl_CloseApplication.Click += new System.EventHandler(this.lbl_CloseApplication_Click);
+      // 
+      // HeadBgd
+      // 
+      this.HeadBgd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(92)))), ((int)(((byte)(73)))), ((int)(((byte)(120)))));
+      this.HeadBgd.Location = new System.Drawing.Point(0, 0);
+      this.HeadBgd.Name = "HeadBgd";
+      this.HeadBgd.Size = new System.Drawing.Size(250, 27);
+      this.HeadBgd.TabIndex = 11;
+      this.HeadBgd.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+      this.HeadBgd.MouseDown += new System.Windows.Forms.MouseEventHandler(this.HeadBgd_MouseDown);
+      this.HeadBgd.MouseMove += new System.Windows.Forms.MouseEventHandler(this.HeadBgd_MouseMove);
+      // 
+      // Updater
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(71)))), ((int)(((byte)(94)))));
+      this.ClientSize = new System.Drawing.Size(250, 105);
+      this.Controls.Add(this.UpdateBtn);
+      this.Controls.Add(this.LatestVersionLabel);
+      this.Controls.Add(this.CurrentVersionLabel);
+      this.Controls.Add(this.lbl_DownloadName);
+      this.Controls.Add(this.UpdatePgb);
+      this.Controls.Add(this.TitleLbl);
+      this.Controls.Add(this.lbl_CloseApplication);
+      this.Controls.Add(this.HeadBgd);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+      this.Name = "Updater";
+      this.Text = "客户端更新器";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button UpdateBtn;
+    private System.Windows.Forms.Label LatestVersionLabel;
+    private System.Windows.Forms.Label CurrentVersionLabel;
+    private System.Windows.Forms.Label lbl_DownloadName;
+    private System.Windows.Forms.ProgressBar UpdatePgb;
+    private System.Windows.Forms.Label TitleLbl;
+    private System.Windows.Forms.Label lbl_CloseApplication;
+    private System.Windows.Forms.Label HeadBgd;
+  }
+}
+
---
+++ FileUpdater/Updater.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace FileUpdater {
+  public partial class Updater : Form {
+    public Updater() {
+      InitializeComponent();
+    }
+
+    private void lbl_CloseApplication_Click(object sender, EventArgs e) {
+      Application.Exit();
+    }
+    #region 移动窗口
+    private Point mPoint = new Point();
+    private void HeadBgd_MouseDown(object sender, MouseEventArgs e) {
+      mPoint.X = e.X;
+      mPoint.Y = e.Y;
+    }
+    #endregion
+
+    private void HeadBgd_MouseMove(object sender, MouseEventArgs e) {
+      if (e.Button == MouseButtons.Left) {
+        Point myPosittion = MousePosition;
+        myPosittion.Offset(-mPoint.X, -mPoint.Y);
+        Location = myPosittion;
+      }
+    }
+    
+    private void UpdateBtn_Click(object sender, EventArgs e) {
+      Newtonsoft.Json.JsonConvert.SerializeObject(new object());
+    }
+  }
+}
