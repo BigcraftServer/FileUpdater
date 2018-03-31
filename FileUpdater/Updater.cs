c69c81a7a5c117d706d4d68376f92dabeaf3c43a
Guoyongchang@WinToGo <guoyongchang@saohuo77.com>
2018-03-31 13:09:30 +0800

    '重构结束'

--- FileUpdater/Updater.cs
+++ FileUpdater/Updater.cs
@@ -13,13 +13,11 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using static FileUpdater.UpdaterCore;
 
 namespace FileUpdater {
   public partial class Updater : Form {
-    private ClientConfig clientConfig;
-    private ServerConfig serverConfig;
-    private IList<Models.Directory> clientDirectories;
+    //Environment.GetCommandLineArgs()?[0]
+    private UpdaterCore updaterCore = UpdaterCore.Instance();
     public Updater() {
       InitializeComponent();
       // 允许随便更改控件
@@ -55,14 +53,9 @@ namespace FileUpdater {
     /// <param name="sender"></param>
     /// <param name="e"></param>
     private void Updater_Load(object sender, EventArgs e) {
-      LoadClientConfig();
+      updaterCore.LoadClientConfig().GenerateDebugFolder();
       //展示本地版本
-      this.CurrentVersionLabel.Text = clientConfig.CurrentVersion;
-
-      if (clientConfig.Debug.HasValue && clientConfig.Debug.Value) {
-        clientConfig.Debug = false;
-        LoadClientDirectories();
-      }
+      this.CurrentVersionLabel.Text = updaterCore.clientConfig.CurrentVersion;
     }
     /// <summary>
     /// 窗体显示事件
@@ -70,7 +63,9 @@ namespace FileUpdater {
     /// <param name="sender"></param>
     /// <param name="e"></param>
     private void Updater_Shown(object sender, EventArgs e) {
-      LoadServerConfig();
+      updaterCore.LoadServerConfig().LoadClientDirectories().InitIcon();
+      DisplayIcon();
+
       CheckUpdate();
     }
     /// <summary>
@@ -87,150 +82,12 @@ namespace FileUpdater {
     /// <param name="sender"></param>
     /// <param name="e"></param>
     private void Updater_FormClosing(object sender, FormClosingEventArgs e) {
-      FileHelper.WriteToFile(clientConfig, "updater.json");
-    }
-    #endregion
-
-    #region 加载Config
-    /// <summary>
-    /// 加载客户端Config
-    /// </summary>
-    /// <param name="fileName"></param>
-    //private void LoadClientConfig(string fileName = "./updater.json") {
-    //  GenerateDefaultConfig(isForce: true);
-    //  using (StreamReader fs = new StreamReader(fileName, Encoding.UTF8)) {
-    //    this.clientConfig = JsonConvert.DeserializeObject<ClientConfig>(fs.ReadToEnd());
-    //  }
-    //}
-    //private void GenerateDefaultConfig(string fileName = "./updater.json", bool isForce = false) {
-    //  if (!System.IO.File.Exists(fileName) || isForce) {
-    //    ClientConfig defaultSettings = JsonConvert.DeserializeObject<ClientConfig>(Resources.DefaultSettings);
-    //    FileHelper.WriteToFile(defaultSettings, fileName);
-    //  }
-    //}
-    /// <summary>
-    /// 加载客户端Files文件
-    /// </summary>
-    private void LoadClientDirectories() {
-      this.clientDirectories = FileHelper.GetDirectories(this.serverConfig.Directories.Select(c => c.DirName).ToArray()).GroupBy(c => c.DirName).Select(c => c.First()).ToList();
-    }
-    /// <summary>
-    /// 加载服务端Config
-    /// </summary>
-    private void LoadServerConfig() {
-      try {
-        Uri serverConfigUri = new Uri($"{this.clientConfig.ServerUrl}{this.clientConfig.MainFile}?r={Guid.NewGuid().ToString()}");
-        this.serverConfig = HttpHelper.GetJsonFile<ServerConfig>(serverConfigUri);
-        LoadServerDirectories();
-        LoadClientDirectories();
-        LoadIcon();
-      } catch (Exception) {
-        this.LatestVersionLabel.Text = "未连接到远程服务器";
-        this.LatestVersionLabel.ForeColor = Color.Red;
-        throw;
-
-      }
-    }
-    /// <summary>
-    /// 加载服务端Files文件
-    /// </summary>
-    private void LoadServerDirectories() {
-      bool isAbsolutePath = Uri.TryCreate(serverConfig.FileAddress, UriKind.Absolute, out Uri uri);
-      IList<Models.Directory> directories = null;
-      if (isAbsolutePath) {
-        directories = HttpHelper.GetJsonFile<List<Models.Directory>>(uri);
-      } else {
-        bool isRelativePath = Uri.TryCreate($"{clientConfig.ServerUrl}{serverConfig.FileAddress}", UriKind.Absolute, out uri);
-        if (isRelativePath) {
-          directories = HttpHelper.GetJsonFile<List<Models.Directory>>(uri);
-        } else {
-          throw new Exception($"去它妈的什么路径?{clientConfig.ServerUrl}{serverConfig.FileAddress}");
-        }
-      }
-      this.serverConfig.Directories = directories;
-    }
-    private void LoadIcon(bool forceUpdate = false) {
-      if (forceUpdate || (!string.IsNullOrEmpty(serverConfig.Icon) && !clientConfig.Icon.Equals(serverConfig.Icon))) {
-        bool isAbsolutePath = Uri.TryCreate(serverConfig.Icon, UriKind.Absolute, out Uri uri);
-        byte[] iconBytes;
-        if (isAbsolutePath) {
-          iconBytes = HttpHelper.GetBytes(uri);
-        } else {
-          bool isRelativePath = Uri.TryCreate($"{clientConfig.ServerUrl}{serverConfig.Icon}", UriKind.Absolute, out uri);
-          if (isRelativePath) {
-            iconBytes = HttpHelper.GetBytes(uri);
-          } else {
-            throw new Exception($"去它妈的什么路径?{clientConfig.ServerUrl}{serverConfig.Icon}");
-          }
-        }
-        System.IO.File.WriteAllBytes("icon.ico", iconBytes);
-        clientConfig.Icon = serverConfig.Icon;
-        DisplayIcon(iconBytes);
-      } else {
-        clientConfig.Icon = serverConfig.Icon;
-        DisplayIcon(Convert.FromBase64String(clientConfig.Base64Icon));
-      }
-
-    }
-    #endregion
-
-    #region 对比差异
-    /// <summary>
-    /// 获取目录相对关系 方便使用
-    /// </summary>
-    /// <returns></returns>
-    private IEnumerable<Tuple<string, IList<Models.File>, IList<Models.File>>> GetReleativeDirectories() {
-      var releativeDirs =
-        from serverDir in serverConfig.Directories
-        join clientDir in this.clientDirectories on serverDir.DirName equals clientDir.DirName
-        select new Tuple<string, IList<Models.File>, IList<Models.File>>(serverDir.DirName, serverDir.Files, clientDir.Files);
-      return releativeDirs;
-    }
-    /// <summary>
-    /// 更新
-    /// </summary>
-    private void Upgrading() {
-      IList<Task> deleteTasks = new List<Task>();
-      IList<Task> downloadTasks = new List<Task>();
-      foreach (var releativeDirectory in GetReleativeDirectories()) {
-        foreach (var file in releativeDirectory.Item2) {
-          switch (file.Event) {
-            case Enum.FileEventEnum.Add:
-              bool fileExists = FileHelper.CheckFileExists(file, releativeDirectory.Item1);
-              if (!fileExists) {
-
-                deleteTasks.Add(new Task(() => {
-                  this.DownloadFileName.Text = $"删除{file.Name}中...";
-                  FileHelper.Delete(file, releativeDirectory.Item1);
-                }));
-
-                downloadTasks.Add(new Task(() => {
-                  this.DownloadFileName.Text = $"下载{file.Name}中...";
-                  HttpHelper.DownloadFile(serverConfig, file, releativeDirectory.Item1, this.UpdatePgb);
-                }));
-              }
-              break;
-            case Enum.FileEventEnum.Delete:
-              deleteTasks.Add(new Task(() => {
-                this.DownloadFileName.Text = $"删除{file.Name}中...";
-                FileHelper.Delete(file, releativeDirectory.Item1);
-              }));
-              break;
-          }
-        }
-      }
-      foreach (var deleteTask in deleteTasks) {
-        deleteTask.RunSynchronously();
-      }
-      foreach (var downloadTask in downloadTasks) {
-        downloadTask.RunSynchronously();
-      }
-      UpdateSuccessful();
+      updaterCore.SaveClientConfig();
     }
     #endregion
 
     #region 提示框简单类
-    public static void ShowMessageBox(bool? isError, string showText, bool showFinelExit) {
+    private static void ShowMessageBox(string showText, bool? isError = null) {
       if (!isError.HasValue) {
         MessageBox.Show(showText, "警告", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
       } else {
@@ -243,22 +100,17 @@ namespace FileUpdater {
     }
     #endregion
 
-    #region 读取本地数据生成Json
-    //private void GenerateLocalFiles() {
-    //  FileHelper.WriteToFile(FileHelper.GetDirectories(clientConfig.GenerateDir.ToArray(), Enum.FileEventEnum.Add), "./serverfiles.json");
-    //}
-    #endregion
-
     #region 更新检测
     private void CheckUpdate(bool forceUpdate = false) {
+      (bool needUpdate, IList<(Models.File, string)> deleteFiles, IList<(Models.File, string)> downloadFiles) = updaterCore.VersionCheck(forceUpdate);
       DownloadFileName.Visible = true;
       DownloadFileName.ForeColor = Color.DarkOrange;
       DownloadFileName.Text = "检查中...";
-      LatestVersionLabel.Text = serverConfig.LatestVersion;
-      if (forceUpdate || !clientConfig.CurrentVersion.Equals(serverConfig.LatestVersion)) {
+      LatestVersionLabel.Text = updaterCore.serverConfig.LatestVersion;
+      if (needUpdate) {
         LatestVersionLabel.ForeColor = Color.Red;
-        if (MessageBox.Show(string.Format("发现客户端新版本:{0}\n更新内容:\n{1}\n是否更新?", serverConfig.LatestVersion, serverConfig.Remark), "发现新版本", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK) {
-          Upgrading();
+        if (MessageBox.Show($"发现客户端新版本:{updaterCore.serverConfig.LatestVersion}\n更新内容:\n{updaterCore.serverConfig.Remark}\n是否更新?", "发现新版本", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK) {
+          Upgrading(deleteFiles, downloadFiles);
           LatestVersionLabel.ForeColor = Color.PaleGreen;
         } else {
           UpdateCanceled();
@@ -273,30 +125,45 @@ namespace FileUpdater {
     }
     private void UpdateSuccessful(bool noUpdated = false) {
       if (!noUpdated) {
-        clientConfig.CurrentVersion = serverConfig.LatestVersion;
-        this.CurrentVersionLabel.Text = clientConfig.CurrentVersion;
-
+        updaterCore.clientConfig.CurrentVersion = updaterCore.serverConfig.LatestVersion;
+        this.CurrentVersionLabel.Text = updaterCore.clientConfig.CurrentVersion;
         DownloadFileName.Text = $"完毕,可以愉悦的启动游戏了";
         DownloadFileName.ForeColor = Color.PaleGreen;
         LatestVersionLabel.ForeColor = Color.PaleGreen;
-
-        ShowMessageBox(false, "更新完成!", false);
+        ShowMessageBox("更新完成!", false);
       } else {
         DownloadFileName.Text = $"已经是最新版本了";
         DownloadFileName.ForeColor = Color.PaleGreen;
         LatestVersionLabel.ForeColor = Color.PaleGreen;
       }
     }
-    #endregion
+    private void Upgrading(IList<(Models.File, string)> deleteFiles, IList<(Models.File, string)> downloadFiles) {
+      IList<Task> deleteTasks = deleteFiles.Select(file => {
+        return new Task(() => {
+          this.DownloadFileName.Text = $"删除{file.Item1.Name}中...";
+          FileHelper.Delete(file.Item1, file.Item2);
+        });
+      }).ToList();
+      IList<Task> downloadTasks = downloadFiles.Select(file => {
+        return new Task(() => {
+          this.DownloadFileName.Text = $"下载{file.Item1.Name}中...";
+          HttpHelper.DownloadFile(updaterCore.serverConfig, file.Item1, file.Item2, this.UpdatePgb);
+        });
+      }).ToList();
 
-    #region 图标展示
-    private void DisplayIcon(byte[] iconBytes) {
-      clientConfig.Base64Icon = Convert.ToBase64String(iconBytes);
-      using (MemoryStream ms = new MemoryStream(iconBytes)) {
-        Icon icon = new Icon(ms);
-        this.Icon = icon;
-        HeadIcon.Image = Bitmap.FromHicon(icon.Handle);
+      foreach (var deleteTask in deleteTasks) {
+        deleteTask.RunSynchronously();
       }
+      foreach (var downloadTask in downloadTasks) {
+        downloadTask.RunSynchronously();
+      }
+      UpdateSuccessful();
+    }
+    #endregion
+    #region 图标
+    private void DisplayIcon() {
+      this.Icon = updaterCore.Icon.Value;
+      this.HeadIcon.Image = updaterCore.Icon.Value.ToBitmap();
     }
     #endregion
   }
--- FileUpdater/UpdaterCore.cs
+++ FileUpdater/UpdaterCore.cs
@@ -12,16 +12,33 @@ using System.Windows.Forms;
 
 namespace FileUpdater {
   public class UpdaterCore {
-    private static UpdaterCore updaterCore;
-    public static UpdaterCore GetUpdaterCore() {
-      if (updaterCore == null)
-        updaterCore = new UpdaterCore();
-      return updaterCore;
+    private static UpdaterCore instance;
+    private UpdaterCore(string path) {
+      this.clientConfigPath = path;
     }
+    public static UpdaterCore Instance(string path = "./config.json") {
+      if (instance == null) {
+        instance = new UpdaterCore(path);
+      }
+      return instance;
+    }
+
+    #region properties
     /// <summary>
-    /// 图标
+    /// 客户端配置文件地址(相对)
     /// </summary>
-    public static Lazy<Icon> Icon {
+    public string clientConfigPath;
+    /// <summary>
+    /// 服务端Uri
+    /// </summary>
+    public Lazy<Uri> serverUri {
+      get {
+        return new Lazy<Uri>(() => {
+          return new Uri($"{clientConfig.ServerUrl}{clientConfig.MainFile}?r={Guid.NewGuid().ToString()}");
+        });
+      }
+    }
+    public Lazy<Icon> Icon {
       get {
         return new Lazy<Icon>(() => {
           Icon icon = null;
@@ -35,167 +52,15 @@ namespace FileUpdater {
     /// <summary>
     /// 客户端配置文件
     /// </summary>
-    public static ClientConfig clientConfig;
+    public ClientConfig clientConfig;
     /// <summary>
     /// 服务端配置文件
     /// </summary>
-    public static ServerConfig serverConfig;
-    public static string clientConfigPath;
+    public ServerConfig serverConfig;
     /// <summary>
     /// 客户端文件列表
     /// </summary>
-    public static IList<Models.Directory> clientDirectories;
-    #region ClientConfig
-    /// <summary>
-    /// 加载客户端配置文件
-    /// </summary>
-    public static UpdaterCore LoadClientConfig(string fileName = "updater.json", bool isFixed = false) {
-      clientConfigPath = fileName;
-      CheckAndGenerateClinetConfig(fileName, false);
-      string clientConfigFromFile = FileHelper.ReadFromFile(fileName);
-      try {
-        clientConfig = JsonConvert.DeserializeObject<ClientConfig>(clientConfigFromFile);
-      } catch (Exception) {
-        if (!isFixed) {
-          FixClientConfig(fileName);
-          return LoadClientConfig(fileName, true);
-        }
-        throw;
-      }
-      return updaterCore;
-    }
-    /// <summary>
-    /// 加载客户端文件列表
-    /// </summary>
-    public static UpdaterCore LoadClientDirectories() {
-      clientDirectories = FileHelper.GetDirectories(serverConfig.Directories.Select(c => c.DirName).ToArray()).GroupBy(c => c.DirName).Select(c => c.First()).ToList();
-      if (clientConfig.Debug.HasValue && clientConfig.Debug.Value)
-        WriteFileForDebug();
-      return updaterCore;
-    }
-    public static UpdaterCore UpdateClientConfig() {
-      clientConfig.CurrentVersion = serverConfig.LatestVersion;
-      clientConfig.Debug = false;
-      clientConfig.Icon = serverConfig.Icon;
-      return updaterCore;
-    }
-    public static UpdaterCore SaveClientConfig() {
-      FileHelper.WriteToFile(clientConfig, clientConfigPath);
-      return updaterCore;
-    }
-    private static void WriteFileForDebug() {
-      FileHelper.WriteToFile(FileHelper.GetDirectories(clientConfig.GenerateDir.ToArray(), Enum.FileEventEnum.Add), "./serverfiles.json");
-    }
-    /// <summary>
-    /// 修复客户端
-    /// </summary>
-    /// <param name="fileName"></param>
-    /// <returns></returns>
-    private static void FixClientConfig(string fileName) {
-      var breakedFileName = BackupClientConfig(fileName);
-      CheckAndGenerateClinetConfig(fileName);
-    }
-    /// <summary>
-    /// 生成默认配置文件
-    /// </summary>
-    /// <param name="fileName"></param>
-    /// <param name="forceGenerate"></param>
-    private static void CheckAndGenerateClinetConfig(string fileName, bool forceGenerate = false) {
-      if (!System.IO.File.Exists(fileName) || forceGenerate) {
-        ClientConfig defaultSettings = JsonConvert.DeserializeObject<ClientConfig>(Resources.DefaultSettings);
-        FileHelper.WriteToFile(defaultSettings, fileName);
-      }
-    }
-    /// <summary>
-    /// 备份配置文件
-    /// </summary>
-    /// <param name="fileName"></param>
-    /// <returns></returns>
-    private static string BackupClientConfig(string fileName) {
-      var newFileName = Path.GetFileNameWithoutExtension(fileName) + DateTime.Now.ToString("yyyyMMddhhmmss") + "_bak" + Path.GetExtension(fileName);
-      System.IO.File.Move(fileName, newFileName);
-      return newFileName;
-    }
-    #endregion
-    #region ServerConfig
-    /// <summary>
-    /// 服务端Uri
-    /// </summary>
-    private static Lazy<Uri> serverUri {
-      get {
-        return new Lazy<Uri>(() => {
-          return new Uri($"{clientConfig.ServerUrl}{clientConfig.MainFile}?r={Guid.NewGuid().ToString()}");
-        });
-      }
-    }
-    /// <summary>
-    /// 加载服务端地址
-    /// </summary>
-    public static UpdaterCore LoadServerConfig() {
-      try {
-        serverConfig = HttpHelper.GetJsonFile<ServerConfig>(serverUri.Value);
-        LoadServerDirectories();
-      } catch (Exception) {
-        throw;
-      }
-      return updaterCore;
-    }
-    /// <summary>
-    /// 加载图标
-    /// </summary>
-    /// <param name="forceUpdate"></param>
-    /// <returns></returns>
-    public static UpdaterCore InitIcon(bool forceUpdate = false) {
-      if (forceUpdate || (!string.IsNullOrEmpty(serverConfig.Icon) && !clientConfig.Icon.Equals(serverConfig.Icon))) {
-        bool isAbsolutePath = Uri.TryCreate(serverConfig.Icon, UriKind.Absolute, out Uri uri);
-        byte[] iconBytes;
-        if (isAbsolutePath) {
-          iconBytes = HttpHelper.GetBytes(uri);
-        } else {
-          bool isRelativePath = Uri.TryCreate($"{clientConfig.ServerUrl}{serverConfig.Icon}", UriKind.Absolute, out uri);
-          if (isRelativePath) {
-            iconBytes = HttpHelper.GetBytes(uri);
-          } else {
-            throw new Exception($"去它妈的什么路径?\n{clientConfig.ServerUrl}{serverConfig.Icon}");
-          }
-        }
-        clientConfig.Base64Icon = Convert.ToBase64String(iconBytes);
-      }
-      return updaterCore;
-    }
-    /// <summary>
-    /// 加载服务端文件列表
-    /// </summary>
-    /// <param name="serverFileAddress"></param>
-    private static void LoadServerDirectories() {
-      string serverFileAddress = serverConfig.FileAddress;
-      bool isAbsolutePath = Uri.TryCreate(serverFileAddress, UriKind.Absolute, out Uri uri);
-      IList<Models.Directory> directories = null;
-      if (isAbsolutePath) {
-        directories = HttpHelper.GetJsonFile<List<Models.Directory>>(uri);
-      } else {
-        bool isRelativePath = Uri.TryCreate(clientConfig.ServerUrl + serverFileAddress, UriKind.Absolute, out uri);
-        if (isRelativePath) {
-          directories = HttpHelper.GetJsonFile<List<Models.Directory>>(uri);
-        } else {
-          throw new Exception($"去它妈的什么路径?\n{serverFileAddress}");
-        }
-      }
-      serverConfig.Directories = directories;
-    }
-    #endregion
-    #region Core
-    /// <summary>
-    /// 获取相对应文件夹
-    /// </summary>
-    /// <returns></returns>
-    private static IEnumerable<Tuple<string, IList<Models.File>, IList<Models.File>>> GetReleativeDirectories() {
-      var releativeDirs =
-        from serverDir in serverConfig.Directories
-        join clientDir in clientDirectories on serverDir.DirName equals clientDir.DirName
-        select new Tuple<string, IList<Models.File>, IList<Models.File>>(serverDir.DirName, serverDir.Files, clientDir.Files);
-      return releativeDirs;
-    }
+    public IList<Models.Directory> clientDirectories;
     #endregion
   }
 }
